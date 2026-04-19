98635684561c598928d9abde0e2a772272ab0715
Mradul <mradulmahajan@gmail.com>
2026-04-19 14:02:19 +0530

    fix cors

--- backend/run.py
+++ backend/run.py
@@ -4,7 +4,9 @@ from app.config import Config
 from flask_cors import CORS
 
 app = create_app()
-CORS(app, resources={r"/*": {"origins": Config.ALLOWED_ORIGINS}}, supports_credentials=True)
+
+# Allow all origins for production connectivity
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(Config.PORT), debug=(Config.FLASK_ENV == "development"))
